94062d83c913687863453135b00fc71517c8642b
YichenZHAO <zhaoyc86@hotmail.com>
2014-09-17 10:49:32 -0700

    hw2_Sep17

--- hw2.R
+++ hw2.R
@@ -3,9 +3,11 @@
 #Session -> Set Working Directory -> Choose Directory
 
 gData_delim <- read.delim("gapminderDataFiveYear.txt")
-gData_table <- read.table("gapminderDataFiveYear.txt")
+gData_table <- read.table("gapminderDataFiveYear.txt", header = TRUE, sep = "\t", 
+                              quote = "\", fill = TRUE, comment.char = "")
 
 #Read data online
+
 gData <- read.delim("gapminderDataFiveYear.txt")
 
 
